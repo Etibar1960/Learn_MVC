12a2ebfbe864eddd1eb375227b6d26860e48cd30
Etibar <kerimov_etibar@mail.ru>
2018-03-10 14:59:43 +0400

    Composer

--- vendor/core/Db.php
+++ vendor/core/Db.php
@@ -1,18 +1,36 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace vendor\core;
 
-namespace vendor\core\base;
-
-/**
- * Description of Db
- *
- * @author Etibar
- */
 class Db {
-    //put your code here
+
+    protected $pdo;
+    protected static $instance;
+
+    protected function __construct() {
+        $db = require ROOT . '/config/config_db.php';
+        $this->pdo = new \PDO($db['dsn'], $db['user'], $db['pass']);
+    }
+
+    public static function instance() {
+        if (self::$instance === NULL) {
+            self::$instance = new self;
+        }
+        return self::$instance;
+    }
+
+    public function execute($sql) {
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute();
+    }
+
+    public function query($sql) {
+        $stmt = $this->pdo->prepare($sql);
+        $res = $stmt->execute();
+        if ($res !== FALSE) {
+            return $stmt->fetchAll();
+        }
+        return [];
+    }
+
 }
